c9847035d20333ddb7dc513e2bbee4b8a7b7a3f5
Alexander2116 <Aleksander2116k@gmail.com>
2022-11-19 13:17:08 +0000

    py + csv
    Fit a function to the bulb spectra.

---
+++ Fit_function.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 19 12:54:11 2022
+
+Fits function to bulb spectra.
+It will allow to find a fuction describing bulb spectra.
+The data is taken from range 330nm and 630nm with step 0.04nm
+
+@author: Alex
+"""
+
